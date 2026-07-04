ebc3e9a8ad2fc31c0e150b7d9ab0b484fad44b07
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-04 10:43:08 +0100

    build: routes for url parts

--- projectpilot/urls.py
+++ projectpilot/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+#Routes for project pages 
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users import views as user_views 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", user_views.dashboard, name="dashboard"),
+    path("users/", include('users.urls')),
+    path("projects/", include('projects.urls')),
+    path("inbox/", include('inbox.urls')),
+
+    path("accounts/", include("django.contrib.auth.urls")),
+]
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
