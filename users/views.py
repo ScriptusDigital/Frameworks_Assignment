012455f1d44f26f7e3b34618a6ffa168b8f4ab87
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-04 13:17:41 +0100

    build: basic user operations inc. login/out/reg

--- users/views.py
+++ users/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
 
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import redirect, render
+
+from users.forms import UserRegisterForm
+
+@login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
 
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})     
+
+
+@login_required
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'users/profile.html')
