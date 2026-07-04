3fafaf19361c04dfc383b6bdf07b492426721d9e
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-04 09:26:48 +0100

    fix: changing wrong project name

--- projectpilot/wsgi.py
+++ projectpilot/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for studentpilot project.
+WSGI config for projectpilot project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentpilot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectpilot.settings')
 
 application = get_wsgi_application()
