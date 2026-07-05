3c7098ec8de808c2b35762312c6af1b55adbb9a6
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-05 09:50:03 +0100

    build: defining fields in form for projects

---
+++ projects/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Project
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name', 'description', 'start_date', 'end_date', 'stakeholders', 'status', 'category']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'stakeholders': forms.Textarea(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+#Make sure end date is not before start date
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+
+        if start_date and end_date and start_date > end_date:
+            raise forms.ValidationError("End date should be greater than or equal to start date.")
+        
+        return cleaned_data
--- projects/models.py
+++ projects/models.py
@@ -34,5 +34,5 @@ class Project(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     
-
- 
+    def __str__(self):
+        return self.name    
