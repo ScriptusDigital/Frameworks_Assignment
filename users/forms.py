e54f28bfdb031be9939ef7789166656e9cbbc7e9
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-05 09:03:07 +0100

    refactor: updating form for registration to improve xperience

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,23 @@ from django.contrib.auth.models import User
 
 #User Registration  
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(
+        label='Username',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'})
+    )
+    email = forms.EmailField(
+        label='Email',
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+    )
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'})
+    )
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm your password'})
+    )
 
     class Meta:
         model = User
--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import redirect, render
 
-from users.forms import UserRegisterForm
+from .forms import UserRegisterForm
 
 @login_required
 def dashboard(request):
