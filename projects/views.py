c6c559b93d0d181e286a1af7cfd3ff5fe936e937
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-05 10:27:10 +0100

    test: basic wiring for project sections

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import ProjectForm
+from .models import Project
 
 
-def project_list(request):
-    return render(request, 'projects/project_list.html')
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = 'projects/project_list.html'
+    context_object_name = 'projects'
+
+class ProjectDetailView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = 'projects/project_detail.html'
+    context_object_name = 'project'
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = 'projects/project_form.html'
+    form_class = ProjectForm
+    success_url = '/projects/'
+
+class ProjectUpdateView(LoginRequiredMixin, UpdateView):
+    model = Project
+    template_name = 'projects/project_detail.html'
+    form_class = ProjectForm
+    success_url = '/projects/'
+
+class ProjectDeleteView(LoginRequiredMixin, DeleteView):
+    model = Project
+    template_name = 'projects/project_confirm_delete.html'
+    success_url = '/projects/'
