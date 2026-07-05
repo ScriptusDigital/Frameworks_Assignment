b7757b0471c0e3a3a809164c834d7fab6e850a0c
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-05 09:35:33 +0100

    feat: creating projects data model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+# Captured data for each project
+class Project(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    stakeholders = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='planning')
+
+    STAUS_CHOICES = [
+        ('planning', 'Planning'),
+        ('active', 'Active'),
+        ('on_hold', 'On Hold'),
+        ('completed', 'Completed'),
+    ]
