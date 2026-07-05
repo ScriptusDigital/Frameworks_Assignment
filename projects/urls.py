afbbb9954e37895289c3e40f2ef4a6618a1be17e
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-05 10:26:42 +0100

    build: defining project url links to sections

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from . import views
+from . views import (
+    ProjectCreateView,
+    ProjectListView,
+    ProjectDetailView,
+    ProjectUpdateView,
+    ProjectDeleteView,
+)
 
 urlpatterns = [
-    path('', views.project_list, name='project_list'),
+    path("", ProjectListView.as_view(), name="project_list"),
+    path("project/<int:pk>/", ProjectDetailView.as_view(), name="project_detail"),
+    path("project/new/", ProjectCreateView.as_view(), name="project_create"),
+    path("project/<int:pk>/update/", ProjectUpdateView.as_view(), name="project_update"),
+    path("project/<int:pk>/delete/", ProjectDeleteView.as_view(), name="project_delete")
 ]
