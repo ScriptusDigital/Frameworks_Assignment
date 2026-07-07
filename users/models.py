f29a49f30ee72d51a56b104beffcc7b4bad4aeb6
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-07 15:06:11 +0100

    fix: corrected errors in model structure and syntax

--- users/models.py
+++ users/models.py
@@ -8,8 +8,8 @@ from django.db import models
 # Defined user role choices 
 
 ROLE_CHOICES = [
-    ("user", "User")
-    ("manager", "Project Manager")
+    ("user", "User"),
+    ("manager", "Project Manager"),
     ("admin", "Administrator")
 
 ]
@@ -18,9 +18,9 @@ class Profile(models.Model):
 # Based on walkthrough https://www.crunchydata.com/blog/extending-djangos-user-model-with-onetoonefield 
 # and https://docs.djangoproject.com/en/6.0/topics/db/examples/one_to_one/#
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=30, black=True)
+    phone = models.CharField(max_length=30, blank=True)
     department = models.CharField(max_length=100, blank=True)
-    role = models.CharField(max_length=20), choices=ROLE_CHOICES, default="user"
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="user")
 
     def __str_(self):
         return f"{self.user.username} profile"
