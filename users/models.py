4befb5a4c1459ec5a81dbaed0dbab491d1f49a8b
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-07 15:00:56 +0100

    build: adding db user profile values

--- users/models.py
+++ users/models.py
@@ -5,9 +5,13 @@ from django.db import models
 # User model
 
 class Profile(models.Model):
+# Based on walkthrough https://www.crunchydata.com/blog/extending-djangos-user-model-with-onetoonefield 
+# and https://docs.djangoproject.com/en/6.0/topics/db/examples/one_to_one/#
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=30, black=True)
+    department = models.CharField(max_length=100, blank=True)
+    role = models.CharField(max_length=20)
 
-    user
-    phone 
-    department 
-    role 
-    
+    def __str_(self):
+        return f"{self.user.username} profile"
+
