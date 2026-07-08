9924cedd3efb3d1f2a2dbdc9faa409a790542f72
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-08 11:07:15 +0100

    feat: adding python logic for user view.py

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import redirect, render
 
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
 from .models import Profile
 
 @login_required
@@ -26,6 +26,27 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})     
 
 
+#Logic for Profile view page updating
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+
+    profile_obj, created = Profile.objects.get_or_create(user=request.user)
+
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, instance=profile_obj)
+
+        if user_form.is_valid() and profile.form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            messages.success(request, "Your profile has been updated.")
+            return redirect("profile")
+    
+    else: 
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=profile_obj)
+
+        
+
+    return render(request, 'users/profile.html',)
