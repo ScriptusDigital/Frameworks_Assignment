90d2ae4b8d336ac9ceb149ecfb0aebdebf026ffa
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-16 12:03:41 +0100

    refactor: change to widgets on form to display empty value suggestion

--- inbox/forms.py
+++ inbox/forms.py
@@ -4,17 +4,18 @@ from .models import Message
 
 #Form for messages
 class MessageForm(forms.ModelForm):
+    recipient = forms.ModelChoiceField(
+        queryset=Message._meta.get_field("recipient").remote_field.model.objects.all(),
+        empty_label="Choose a recipient",
+        widget=forms.Select(attrs={"class": "form-select"})
+    )
     class Meta:
         model = Message
         fields = ["recipient", "subject", "body"]
 
 #Widgets for style
         widgets = {
-            "recipient": forms.Select(
-                attrs={"class": "form-select",
-                       "placeholder": "Choose a recipient"}
-            ),
-
+    
              "subject": forms.TextInput(
                 attrs={
                     "class": "form-control",
