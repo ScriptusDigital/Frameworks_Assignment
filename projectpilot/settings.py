0ba79992f4a675ca073782eadc18932441cef9e4
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-16 09:29:43 +0100

    feat: password reset email link page

--- projectpilot/settings.py
+++ projectpilot/settings.py
@@ -139,15 +139,9 @@ LOGOUT_REDIRECT_URL = 'login'
 
 # Email account for password reset
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
 EMAIL_HOST = "smtp.gmail.com"
-
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
-
 EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
-
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
-
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
