6d4bbfea6d56e726e95f356630f1d8d364d7f10a
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-16 11:32:24 +0100

    style: widget config for message form

--- inbox/forms.py
+++ inbox/forms.py
@@ -7,10 +7,30 @@ class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ["recipient", "subject", "body"]
+
 #Widgets for style
         widgets = {
-            "body": forms.Textarea(attrs={"rows": 5, "id": "messagebody"})
-        
+            "recipient": forms.Select(
+                attrs={"class": "form-select",
+                       "placeholder": "Choose a recipient"}
+            ),
+
+             "subject": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter a subject"
+                }  
+            ),
+
+             "body": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 5,
+                     "id": "messagebody",
+                      "placeholder": "Write your message",
+                }  
+            ),
+
         }
     def clean_subject(self):
         subject = self.cleaned_data.get("subject", "").strip()
