f88100ca4d2b34549408d1b7487cceeab45665c4
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-16 10:48:56 +0100

    chore: migrate inbox model and placeholder directs in view.py

--- inbox/views.py
+++ inbox/views.py
@@ -1,4 +1,34 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+#Display messages
+@login_required
 def inbox(request):
     return render(request, 'inbox.html')
+
+
+#Sent messages
+@login_required
+def sent_messages(request):
+    return render(request, 'inbox/sent.html')
+
+
+#Archived messages view
+@login_required
+def archived_messages(request):
+    return render(request, 'inbox/archived.html')
+
+#Compose messages
+@login_required
+def compose_message(request):
+    return render(request, 'inbox/compose.html')
+
+# Message details
+@login_required
+def message_detail(request, pk):
+        return render(request, 'inbox/message_detail.html', {"message_id":pk},)
+
+ #When archiving messages
+@login_required
+def archive_message(request, pk):
+     return render(request, "inbox/message_detail.html", {"message_id":pk},)
