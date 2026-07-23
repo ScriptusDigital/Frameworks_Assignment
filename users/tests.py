3343034c26fbedb17ef8e543ce29cc4ba7807499
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-07-23 18:18:18 +0100

    test: user test script login requirement

--- users/tests.py
+++ users/tests.py
@@ -16,12 +16,57 @@ class UserFormTest(TestCase):
             data={
                 "username":"newuser",
                 "email":"newuser@example.com",
-                "username":"SecureTestPassword123!",
-                "username":"SecureTestPassword123!",
+                "password":"SecureTestPassword123!",
+
             }
         )
 
         self.assertTrue(form.is_valid())
 
+@override_settings(
+     STORAGES={
+          "staticfiles":{
+               "BACKEND":"django.contrib.staticfiles.storage.StaticFilesStorage",
+          }
+     }
+)
 
-        
+class UserViewTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="profileuser",
+            email="old@example,com",
+            password="testpassword123",
+        )
+
+        self.profile = Profile.objects.create(
+            user=self.user,
+        )
+
+#Reg creates a user and profile
+
+    def test_registration_creates_user_and_profile(self):
+        response = self.client.post(
+            reverse("register"),
+            {
+                "username":"newuser",
+                "email":"newuser@example.com",
+                "password1":"SecureTestPassword123!",
+                "password2":"SecureTestPassword123!",
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+
+        user = User.objects.get(username="registereduser")
+
+        self.assertTrue(
+            Profile.objects.filter(user=user).exists()
+        )
+
+        self.assertTrue(
+            user.check_password("SecureTestPassword123!")
+        )
+
+#Profile requires login
